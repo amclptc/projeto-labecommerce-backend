4322ff74bf0ea39b2e9796857748156fbdcc4179
Andersom Maciel <amclptc@gmail.com>
2023-01-25 17:41:00 -0300

    Foi criado o arquivo package.json e o index.js; testei a aplicação com um console.log e um uso do process.argv; desenvolvi uma aplicação que permite jogar par ou ímpar no terminal.

---
+++ index.js
@@ -0,0 +1,5 @@
+//Labenu: crie o arquivo index.js e dentro dele coloque um console.log avisando que o aplicativo foi iniciado.
+console.log("O aplicativo foi iniciado!");
+
+//Labenu: faça aparecer no console pelo menos um argumento.
+console.log(process.argv[2]);
